1035ca2349feaa112d4074e7bd1b63951c0dc8b9
michael <michael@nothere.com>
2026-06-05 20:54:05 +0200

    xxx

--- package/src/blib5/decorate.py
+++ package/src/blib5/decorate.py
@@ -1,3 +1,7 @@
+from functools import wraps
+import inspect
+import bpy
+from blib3.household import delete_object_and_data_if_orphaned
 
 _missing = object()
 def cache_unary_function_result(f):
@@ -9,4 +13,75 @@ def cache_unary_function_result(f):
         return r
     return f_wrapper
 
+
+def blender_object(select_set=True, active_set=True, delete_object_with_same_name=False, create=False):
+    def decorator(func):
+        sig = inspect.signature(func) 
+        has_kwargs = False
+        param_names = set()
+        for param in sig.parameters.values():
+            if param.kind == inspect.Parameter.VAR_KEYWORD:
+                has_kwargs = True
+            elif param.kind == inspect.Parameter.POSITIONAL_OR_KEYWORD:
+                param_names.add(param.name)
+        @wraps(func)
+        def wrapper(*args, **kwargs) -> bpy.types.Object : 
+            #print("wrapper", args, kwargs)
+            scene = kwargs.pop('scene', bpy.context.scene if create else None)
+            if isinstance(scene, str):
+                    c = bpy.data.scenes(str)
+            c = kwargs.pop('collection', scene.collection if create else None)
+            #c = kwargs.pop('collection', None)
+            m = kwargs.pop('material', None)
+            g = kwargs.pop('geo', None)
+            s = kwargs.pop('select_set', select_set)
+            a = kwargs.pop('active_set', active_set)           
+            d = kwargs.pop('delete_object_with_same_name', delete_object_with_same_name)
+            if d:
+                if (name := kwargs.get('name', None)) and (o := bpy.data.objects.get(name)):
+                    delete_object_and_data_if_orphaned(o)
+            data_attrs = []
+            if not has_kwargs:
+                for k in list(kwargs.keys()):
+                    if not k in param_names:
+                        data_attrs.append( (k, kwargs.pop(k)) )
+            bound_args = sig.bind(*args, **kwargs) 
+            o = func(*bound_args.args, **bound_args.kwargs)
+            for k, v in data_attrs:
+                setattr(o.data, k, v)
+            if c:
+                if isinstance(c, str):
+                    c = bpy.data.collections[c]
+                if not isinstance(c, bpy.types.Collection):
+                    raise TypeError("collection should be of type bpy.types.Collection")
+                c.objects.link(o)
+            if m:
+                if isinstance(m, str):
+                    m = bpy.data.materials[m]
+                if not isinstance(m, bpy.types.Material):
+                    raise TypeError("material should be of type bpy.types.Material")
+                o.data.materials.append(m)
+            if g:   # geo node group
+                node_group = bpy.data.node_groups[g]
+                modifier = o.modifiers.new(name=g, type='NODES')
+                modifier.node_group = node_group
+            if s:
+                if isinstance(s, bpy.types.ViewLayer):
+                    o.select_set(True, view_layer=s)
+                else:
+                    o.select_set(True)
+            if a:
+                if isinstance(a, bpy.types.ViewLayer):
+                    a.objects.active = o
+                else:
+                    bpy.context.view_layer.objects.active = o
+            return o
+        return wrapper
+    return decorator
+   
+
+def blender_object_create(select_set=True, active_set=True, delete_object_with_same_name=True):
+    return blender_object(select_set=select_set, active_set=active_set, delete_object_with_same_name=delete_object_with_same_name, create=True)
+
+             
 
--- package/src/blib5/geo/spline.py
+++ package/src/blib5/geo/spline.py
@@ -1,6 +1,40 @@
 import numpy as np 
 from .. import decorate
 
+def calculator(R):
+    MP1 = R.shape[-1]
+    if R.ndim == 2:
+        def calculator(U):
+            npoints = U.shape[0]
+            '''
+            construct PU with every row i = [1 u[i] u[i]² u[i]³ ... ]
+            U = np.linspace(0,1,5)
+            PU = 
+                [1.       0.       0.       0.      ]
+                [1.       0.25     0.0625   0.015625]
+                [1.       0.5      0.25     0.125   ]
+                [1.       0.75     0.5625   0.421875]
+                [1.       1.       1.       1.      ]]
+            '''        
+            PU = np.empty((npoints, MP1))
+            PU[:] = U[:, np.newaxis]
+            PU[:,0] = 1
+            PU = np.cumprod(PU, axis=1)
+            return PU @ R
+    elif R.ndim == 3:
+        def calculator(IDX, U):
+            npoints = U.shape[0]      
+            PU = np.empty((npoints, MP1))
+            PU[:] = U[:, np.newaxis]
+            PU[:,0] = 1
+            PU = np.cumprod(PU, axis=1)
+            return (PU[:,:,np.newaxis]*R[IDX]).sum(axis=1) 
+        calculator.nsegments = R.shape[0]  
+    calculator.R = R
+    return calculator
+
+
+#############################################################################################
 '''
 Calculate the polynomial coefficients for every control point.
 
@@ -11,20 +45,26 @@ array([[ 1, -3,  3, -1],
        [ 0,  0,  3, -3],
        [ 0,  0,  0,  1]], dtype=int32)
 
-Given M+1 control points, we can then "squash" everything together
-along the axis of degree of t to make an efficient calculator that takes a 
-variable range of u 
 '''
 @decorate.cache_unary_function_result
-def bezier_polynomial_coefficients(M, cache = {}):
-    B = cache.get(M,None)
-    if not B is None: return B    
+def bezier_polynomial_coefficients(M):
     B = np.zeros( (M+1,M+1), dtype=np.int32)
     B[:,-1] = 1
     for i in range(M,0,-1):
         B[i-1] = -B[i]
         B[i-1,:-1] += B[i,1:]
     B = B * np.abs(B[0]).reshape((M+1,1))
-    cache[M] = B
     return B
 
+
+'''
+Construct a point calculator for a fixed set of M+1 control points and variable U.
+The shape of control points is (N, M+1, dimension of points) for N segments.
+'''
+def calculator_1d_bezier(CONTROL_POINTS):
+    ''' shape of control points is (M+1, dimension of points)'''
+    M = CONTROL_POINTS.shape[-2] - 1  # degree of the polynomial
+    B = bezier_polynomial_coefficients(M).transpose()
+    # we squash all terms of the same degree together to make an efficient calculator that takes a variable array of t values.
+    R = B @ CONTROL_POINTS
+    return calculator(R)
